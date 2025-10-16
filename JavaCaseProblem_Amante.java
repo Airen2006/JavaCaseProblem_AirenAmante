af932768aa875f286ddeeb612ec5353387f97adf
Airen T. Amante <amanteairen48@gmail.com>
2025-10-16 22:41:22 +0800

    Add files via upload
    Uploaded Java case problem and output screenshot.

---
+++ JavaCaseProblem_Amante.java
@@ -0,0 +1,15 @@
+public class JavaCaseProblem_Amante {
+    public static void main(String[] args) {
+       
+        String name = "Airen Amante";
+        String idNumber = "2024303249";
+        
+        int num1 = 15;
+        int num2 = 25;
+        int sum = num1 + num2;
+        
+        System.out.println("Name: " + name);
+        System.out.println("ID Number: " + idNumber);
+        System.out.println("Result: The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
